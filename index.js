83ba17baff516be46c06656eb13ebdeee8409e9d
Azumi Callanan <azucm@hotmail.com>
2017-12-06 18:06:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,22 +5,6 @@ function theBeatlesPlay(musicians,instruments){
     }
     return array;
  }
- 
- const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-function johnLennonFacts(){
-  var i=0;
-  while(facts.length>0){
-    facts.push(facts[i]+"!!!");
-    i++;
-  }
-  return facts;
-}
 
 
  
